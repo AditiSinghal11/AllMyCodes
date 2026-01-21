be4d6d2e9b512578915bf394057e8c02baae459a
AditiSinghal11 <aditi247ps@gmail.com>
2026-01-21 11:48:28 +0530

    begginer lab practice

--- haskell/addition.hs
+++ haskell/addition.hs
@@ -1,7 +1,9 @@
-                
+
+main :: IO ()
 main=do
+    let var3=[1..10]
     let var1=1
     let var2=3
-    putStrLn"The addition of the the two numbers is "
-    print(var1+var2)
-    
+    putStrLn "The addition of the the two numbers is "
+    print (var1+var2)
+    print var3
